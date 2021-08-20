55e98179f3eef40ccf6b8882c67b704f648631b4
Sitaram <sitaram.gupta99@gmail.com>
2021-08-20 12:11:08 +0545

    Removed warning by changing the minor code

--- SimpleNetworkCallsExample/ViewController.swift
+++ SimpleNetworkCallsExample/ViewController.swift
@@ -15,26 +15,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let url = "https://jsonplaceholder.typicode.com/users"
+//        let url = "https://jsonplaceholder.typicode.com/users"
         
-//                        getData(from: url)
-//        getDataUsingAlamofire(from: url)
+        //                        getData(from: url)
+        //        getDataUsingAlamofire(from: url)
         
         
-                getDataFromMyData()
+        getDataFromMyData()
         
     }
     
     private   func getDataFromMyData(){
-            getUpdateInfo { (response) in
-                print(response.first?.username)
-                } failure: { (error) in
-                    print(error)
-                    print(error.localizedDescription)
-                }
+        getUpdateInfo { (response) in
+            print(response.first?.username! as Any)
+        } failure: { (error) in
+            print(error)
+            print(error.localizedDescription)
+        }
     }
     
-     func getUpdateInfo(completion: @escaping (_ response: [ApiResponse])->Void, failure: @escaping(_ error: Error)->()){
+    func getUpdateInfo(completion: @escaping (_ response: [ApiResponse])->Void, failure: @escaping(_ error: Error)->()){
         //        return RequestToken.request(urlType: .forceUpdate, method: .get, header: nil, completion: completion)
         AF.request(URLPath.myData.urlString, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil, interceptor: nil, requestModifier: nil).responseData { (response) in
             switch response.result{
@@ -42,7 +42,7 @@ class ViewController: UIViewController {
                 failure(error)
             case .success(let data):
                 do{
-            let parsed = try JSONDecoder().decode([ApiResponse].self, from: data)
+                    let parsed = try JSONDecoder().decode([ApiResponse].self, from: data)
                     completion(parsed)
                 }catch{
                     failure(error)
@@ -90,45 +90,42 @@ class ViewController: UIViewController {
                 //                }
                 
             }
-                
+        
     }
-
-
-
-func getData(from url : String)  {
-    let task =  URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
-        
-        guard let data  = data, error == nil else {
-            print("Something went wrong")
-            return
-        }
-        
-        // have data
-        
-        do{
-            let decoder = JSONDecoder()
-            let result = try decoder.decode([ApiResponse].self, from: data)
-            print(result.first?.username as! String)
-        }
-        catch{
-            print("faild to convert \(error.localizedDescription)")
-        }
-        
-        
-
-        
-//        print(json.name)
-//        print(json.username)
-//        print(json.address?.city)
-//        print(json.address?.geo?.lat)
-//        print(json.company?.name)
-        
-    })
     
-    task.resume()
-}
-
-
+    
+    
+    func getData(from url : String)  {
+        let task =  URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: {data, response, error in
+            
+            guard let data  = data, error == nil else {
+                print("Something went wrong")
+                return
+            }
+            
+            // have data
+            
+            do{
+                let decoder = JSONDecoder()
+                let result = try decoder.decode([ApiResponse].self, from: data)
+                print(result.first?.username as Any)
+            }
+            catch{
+                print("faild to convert \(error.localizedDescription)")
+            }
+            
+            
+            
+            
+            //        print(json.name)
+            //        print(json.username)
+            //        print(json.address?.city)
+            //        print(json.address?.geo?.lat)
+            //        print(json.company?.name)
+            
+        })
+        task.resume()
+    }
 }
 
 
